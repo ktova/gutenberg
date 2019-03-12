72bda4a8edc6dced76bc71e370179ba48b03ef15
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2019-03-12 09:09:32 +0000

    Update packages/e2e-test-utils/src/open-global-inserter.js
    Co-Authored-By: jorgefilipecosta <jorge.costa@developer.pt>

--- packages/e2e-test-utils/src/open-global-inserter.js
+++ packages/e2e-test-utils/src/open-global-inserter.js
@@ -1,7 +1,7 @@
 /**
  * Opens the global block inserter.
  */
-export async function openGlobalInserter() {
+export async function openGlobalBlockInserter() {
 	await page.click( '.edit-post-header [aria-label="Add block"]' );
 	// Waiting here is necessary because sometimes the inserter takes more time to
 	// render than Puppeteer takes to complete the 'click' action
